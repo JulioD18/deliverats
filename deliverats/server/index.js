f480cd5f28223b668075d5df7f423a15624d94e2
Julio Delgado <jd85451@gmail.com>
2023-03-01 21:46:59 -0500

    feat: basic backend setup

---
+++ deliverats/server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const PORT = 3000;
+export const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    else console.log("HTTP server on http://localhost:%s", PORT);
+});
